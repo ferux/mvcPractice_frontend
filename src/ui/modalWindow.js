9c6f63beec75757bc817a5ff32396714f2edd327
Aleksandr Trushkin <3062287+ferux@users.noreply.github.com>
2017-11-03 14:39:28 -0500

    Remove comments

--- src/ui/modalWindow.js
+++ src/ui/modalWindow.js
@@ -2,39 +2,9 @@ import React from 'react';
 import Moment from 'moment';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-//props:
-//show (bool)
-//data -- data to show
-//onCancel -- function to do in case of cancel
-//onSave -- function to do in case of save
-//editFunc -- function to edit variables.
+
 class ModalWindow extends React.Component {
-    //todo: checkbox
-    // getLiElement(e, liStyle) {
-    //     let reqField = false;
-    //     let addText = '';
-    //     let disField = (e.title === "UUID");
-    //     let inputType = typeof e.value === "boolean" ? "checkbox" : "text";
-    //     if (e.title.includes("uuid")) {reqField = true; addText = '*';}
-    //     return (
-    //     <li style={liStyle}>
-    //         <div className="divFloat">
-    //             <div className="input">{e.title}{addText}</div>
-    //             <input 
-    //                 type={inputType}
-    //                 // value={this.props.data[e.title]} 
-    //                 value={e.value} 
-    //                 onChange={(evt) => this.props.onEdit(evt)} 
-    //                 className="inputText" 
-    //                 name={e.title} 
-    //                 alt={e.title} 
-    //                 required={reqField}
-    //                 disabled={disField}
-    //             />
-    //         </div>
-    //     </li>
-    //     );
-    // };
+   
     createElement = (key, value) => {
         if (typeof value === "string") return listTextItem(key, value, this.props.onChange);
         if (typeof value === "number") return listNumberItem(key, value, this.props.onChange);
@@ -100,33 +70,4 @@ const listMomentItem = (key, value, onChangeMoment) => {
         </li>
     )
 }
-
-// <ul style={ulStyle}>
-// {fillMenu.map(e => {
-//     return this.getLiElement(e, liStyle);
-// })}
-// </ul>
-// <div style={buttonStyle}>
-// <button className="button-me ok" onClick={() => this.props.onSave(this.props.data)}>Save</button>
-
-// </div>
-
-// eslint-disable-next-line
-const ulStyle = {
-    listStyleType: "none",
-    width: "100%",
-    padding: 0
-};
-// eslint-disable-next-line
-const liStyle = {
-    paddingTop: 2,
-    marginBottom: 4
-};
-// eslint-disable-next-line
-const buttonStyle = {
-    textAlign: "center",
-    marginLeft: '2px',
-    marginRight: '2px'
-};
-
 export default ModalWindow;
